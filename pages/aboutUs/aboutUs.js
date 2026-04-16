aebef03f35d4929cb5c16cce9ae3c5c75e67586d
CamiGit <camilo.orjuela@hotmail.com>
2026-04-16 14:59:08 -0500

    [

---
+++ pages/aboutUs/aboutUs.js
@@ -0,0 +1,6 @@
+fetch('../../components/footer/footer.html')
+    .then(res => res.text())
+    .then(html => {
+        document.getElementById('footer-placeholder').innerHTML = html;
+    })
+    .catch(err => console.error('Error cargando el footer:', err));
