b13b8b156df9e42bd3a6910a209224c96068c372
CamiGit <camilo.orjuela@hotmail.com>
2026-04-17 22:14:54 -0500

    colores infoIndez

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,6 +1,6 @@
-fetch('components/navbar/navbar.html')
-.then(response => response.text())
-.then(data => {
-    document.getElementById('navbar-container').innerHTML = data;
-})
-.catch(error => console.error('Error loading navbar:', error));
+fetch('components/footer/footer.html')
+    .then(res => res.text())
+    .then(html => {
+        document.getElementById('footer-placeholder').innerHTML = html;
+    })
+    .catch(err => console.error('Error cargando el footer:', err));
