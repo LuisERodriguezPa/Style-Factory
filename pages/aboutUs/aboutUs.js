ea5e05997b639d8a6dd96a5fb00aa627bd214961
CamiGit <camilo.orjuela@hotmail.com>
2026-04-19 16:50:43 -0500

    Ajuste mapa ubicación y renderizado

--- pages/aboutUs/aboutUs.js
+++ pages/aboutUs/aboutUs.js
@@ -1,9 +1,10 @@
-fetch('components/navbar/navbar.html')
+fetch('../../components/navbar/navbar.html')
     .then(res => res.text())
     .then(html => {
         document.getElementById('navbar-placeholder').innerHTML = html;
     })
     .catch(err => console.error('Error cargando el navbar:', err));
+
 
 fetch('../../components/footer/footer.html')
     .then(res => res.text())
