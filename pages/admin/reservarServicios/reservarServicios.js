b43bd8f56b13a25d2876352829b080a64f0ae37e
Luis Rodriguez <luedropa@gmail.com>
2026-04-28 17:11:13 -0500

    agregando el menu superior

--- pages/admin/reservarServicios/reservarServicios.js
+++ pages/admin/reservarServicios/reservarServicios.js
@@ -1,8 +1,21 @@
 const container = document.getElementById('reservas_container');
 const reservas = JSON.parse(localStorage.getItem('reservas')) || [];
 
+const enlaces = document.querySelectorAll('.menu-item');// Seleccionar todos los enlaces con la clase 'menu-item'
 let texto = document.getElementById("texto");
 let contenedorImagen = document.getElementById("contenedor_imagen");
+
+// 2. Añadir un evento 'click' a cada enlace
+enlaces.forEach(enlace => {
+    enlace.addEventListener('click', function() {
+    
+        // 3. Eliminar la clase 'activo' de todos los enlaces
+        enlaces.forEach(link => link.classList.remove('activo'));
+    
+        // 4. Añadir la clase 'activo' al enlace seleccionado
+        this.classList.add('activo');
+    });
+});
 
 function renderizarReservas() {
     if (reservas.length === 0) {
@@ -12,6 +25,7 @@ function renderizarReservas() {
     reservas.forEach(reserva => {
         const contenidoReserva = document.createElement("div")
         contenidoReserva.className = "contenido_reserva"
+        contenidoReserva.classList.add("row");
         const precioFormateado = reserva.precio.toLocaleString('es-CO');
         const titulo = document.createElement("h1");
         titulo.textContent = reserva.nombre;
@@ -23,19 +37,21 @@ function renderizarReservas() {
         imagen.src = reserva.imagen;
        
         const texto = document.createElement("div");
-        texto.className = "texto";
+        texto.classList.add("texto","col");
         texto.appendChild(titulo);
         texto.appendChild(descripcion);
         const contenedorImagen = document.createElement("div")
-        contenedorImagen.className = "contenedor_imagen";
+        contenedorImagen.classList.add("contenedor_imagen");
         contenedorImagen.appendChild(imagen);
         const contenedorPrecio = document.createElement("div");
         contenedorPrecio.className="contenedor_precio";
+        contenedorPrecio.classList.add("row");
         const tituloPrecio = document.createElement("h1");
+        tituloPrecio.classList.add("col")
         tituloPrecio.textContent = reserva.nombre;
         const precio = document.createElement("h1");
-        precio.className ="precio"
-        precio.textContent = precioFormateado;
+        precio.classList.add("precio", "col");
+        precio.textContent = `$${precioFormateado}`;
 
         contenidoReserva.appendChild(texto);
         contenidoReserva.appendChild(contenedorImagen);
@@ -46,7 +62,6 @@ function renderizarReservas() {
         
         
     });
-
   
 }
 document.addEventListener('DOMContentLoaded', renderizarReservas);
