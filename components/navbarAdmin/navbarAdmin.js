d82930198d50750e9e235e3398cf8473d044772f
marcelaW40k <ardilamarcela42@gmail.com>
2026-05-03 14:01:37 -0500

    agregando sidebar al admin

--- components/navbarAdmin/navbarAdmin.js
+++ components/navbarAdmin/navbarAdmin.js
@@ -1,14 +1,29 @@
-function initNavbarAdmin() {
-    const boton = document.querySelector('.boton-hamburguesa');
-    const menu = document.querySelector('#menuLateral');
+const btnToggle = document.querySelector(".btn-toggle");
+const sidebar = document.querySelector(".sidebar");
 
-    boton.addEventListener('mouseenter', () => {
-        const bsOffcanvas = new bootstrap.Offcanvas(menu);
-        bsOffcanvas.show();
-    });
+btnToggle.addEventListener("click", () => {
+  sidebar.classList.toggle("collapsed");
+});   
 
-    menu.addEventListener('mouseleave', () => {
-        const bsOffcanvas = new bootstrap.Offcanvas(menu);
-        bsOffcanvas.hide();
-    });
-}
+
+const links = document.querySelectorAll(".nav-link");
+
+links.forEach(link => {
+  link.addEventListener("click", function () {
+
+    // Quitar active de todos
+    links.forEach(l => l.classList.remove("active"));
+
+    // Agregar active al clickeado
+    this.classList.add("active");
+
+  });
+});
+
+fetch("components/navbarAdmin/navbarAdmin.html")
+  .then(res => res.text())
+  .then(html => {
+    document.getElementById("navbarAdmin-placeholder").innerHTML = html;
+
+    initSidebar(); 
+  });
