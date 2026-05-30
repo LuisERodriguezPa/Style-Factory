40f6ce578c58ffe311ae1b7ff6141ce49a0353d4
Luis Rodriguez <luedropa@gmail.com>
2026-05-30 10:16:13 -0500

    Haciendo pruebas

--- assets/js/main.js
+++ assets/js/main.js
@@ -79,7 +79,7 @@ fetch('/components/navbar/navbar.html')
         enlaces.forEach(enlace => {
             // 2. Obtenemos el nombre del archivo del href del enlace
             let rutaEnlace = enlace.getAttribute('href').split("/").pop();
-            
+            console.log("Comparando:", rutaEnlace, "con", rutaActual);
             console.log("Comparando:", rutaEnlace, "con", rutaActual);
             if(rutaEnlace == rutaActual){
                 enlace.classList.add('active');
